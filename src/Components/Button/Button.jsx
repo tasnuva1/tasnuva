6ce2830f7f8227b74128d2419af22d1cc49ff4de
tasnuva1 <62549329+tasnuva1@users.noreply.github.com>
2020-12-10 23:10:52 +0600

    testing responsiveness

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import "./Button.style.scss";
+import React from 'react';
+import './Button.style.scss';
 
 const Button = ({ children, btn2, image }) => {
   return (
-    <button className={btn2 ? "btn-small btn-ani" : "primary-btn"}>
-      <div className="button-inner">{children}</div>
-      {image && <img src={image} alt="download-from-the-server" />}
+    <button className={btn2 ? 'btn-small btn-ani' : 'primary-btn'}>
+      <div className='button-inner'>{children}</div>
+      {image && <img src={image} alt='download-from-the-server' />}
     </button>
   );
 };
 
-Button.defaultProps = {
-  btn: "primary-btn",
-};
+// Button.defaultProps = {
+//   btn: "primary-btn",
+// };
 
 export default Button;
