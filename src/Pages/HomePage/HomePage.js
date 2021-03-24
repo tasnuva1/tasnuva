10f257306297b341676418fe8ad4a141aa3fc863
tasnuva1 <62549329+tasnuva1@users.noreply.github.com>
2021-03-24 22:29:20 +0600

    Deleted some unnecessary comments

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -34,27 +34,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-
-/////////////////
-// import React, { useEffect, useState } from 'react';
-// import MainContent from '../../Components/MainContent/MainContent';
-// import ProjectsPage from '../ProjectsPage/ProjectsPage';
-// import OnTheWeb from '../OnTheWeb/OnTheWeb';
-// import ContactPage from '../ContactPage/ContactPage';
-// import Footer from '../Footer/Footer';
-// import SubNavigation from '../../Components/SubNavigation/SubNavigation.jsx';
-
-// const HomePage = () => {
-//   return (
-//     <div>
-//       <MainContent />
-//       <ProjectsPage />
-//       <OnTheWeb />
-//       <ContactPage />
-//       <Footer />
-//       <SubNavigation />
-//     </div>
-//   );
-// };
-
-// export default HomePage;
