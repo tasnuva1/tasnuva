26cfd48bf9e72ca2e6d6aaa31c80479b37406d4b
tasnuva1 <fahmidamashura@gmail.com>
2021-09-01 00:14:47 +0600

    Updates

---
+++ precache-manifest.91e0463a014ba911da145a1aa4417feb.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6d52852ac0945691d27c86dfa235a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "516fec7f01ebb600fd16",
+    "url": "./static/css/main.c66283fc.chunk.css"
+  },
+  {
+    "revision": "f25ffa40c1a540f65588",
+    "url": "./static/js/2.046c925e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.046c925e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516fec7f01ebb600fd16",
+    "url": "./static/js/main.7d2ba236.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "586f8f63f9854fcc38540b9be7e86802",
+    "url": "./static/media/DEMO.586f8f63.svg"
+  },
+  {
+    "revision": "70e97d26069ab1239f5ef1c63b0fdc0c",
+    "url": "./static/media/Realhousey1.70e97d26.mp4"
+  },
+  {
+    "revision": "841e38fe0990386adbb98d614b3d1bbd",
+    "url": "./static/media/Realhousey2.841e38fe.mp4"
+  },
+  {
+    "revision": "ff950490df3a13962986eca0111d52f0",
+    "url": "./static/media/Spapshoot.ff950490.mp4"
+  },
+  {
+    "revision": "60db4852caaa81e6a845be3687f9d404",
+    "url": "./static/media/Veplop.60db4852.mp4"
+  },
+  {
+    "revision": "a73fb2a01d283a7224c3f5d00ca85f2d",
+    "url": "./static/media/behance.a73fb2a0.svg"
+  },
+  {
+    "revision": "2fed2f6fc2262a82af828e4ce6e4aaeb",
+    "url": "./static/media/codepen.2fed2f6f.svg"
+  },
+  {
+    "revision": "7e3dc79578f31d9a864c7c5a3700a924",
+    "url": "./static/media/coding-screen.7e3dc795.svg"
+  },
+  {
+    "revision": "39cbc57f5d6a5039ae03e748b85ec953",
+    "url": "./static/media/download-from-server.39cbc57f.svg"
+  },
+  {
+    "revision": "379d1d1c1485bc546da4cb1e7f16bda7",
+    "url": "./static/media/dribbble.379d1d1c.svg"
+  },
+  {
+    "revision": "cee6deafd2dfbaa5a4dbd606cbf3b321",
+    "url": "./static/media/github.cee6deaf.svg"
+  },
+  {
+    "revision": "bd965c8121ea35d35cdc1606a9835e2e",
+    "url": "./static/media/instagram.bd965c81.svg"
+  },
+  {
+    "revision": "12e9c34ea2d6d91c1ee62b67c8d0096d",
+    "url": "./static/media/linkedin.12e9c34e.svg"
+  },
+  {
+    "revision": "6650424270f8cd2e8eccf6f7fd28c71d",
+    "url": "./static/media/logo1.66504242.svg"
+  },
+  {
+    "revision": "32185268538ef8e7edb4f9166206ec49",
+    "url": "./static/media/logo2.32185268.svg"
+  },
+  {
+    "revision": "559b5bfa9d2728619dbeb9cd846864af",
+    "url": "./static/media/logo3.559b5bfa.svg"
+  },
+  {
+    "revision": "b35b4ade6a5ccf0251e82951fc47eb27",
+    "url": "./static/media/logo4.b35b4ade.svg"
+  },
+  {
+    "revision": "edc3e50586e159711b3744d046e64af7",
+    "url": "./static/media/o.edc3e505.svg"
+  },
+  {
+    "revision": "2e4b6bd1a354cf0cc4fc660dc8c1a793",
+    "url": "./static/media/pinterest.2e4b6bd1.svg"
+  },
+  {
+    "revision": "076e7b4e5cfe190a2272195ed6e1be05",
+    "url": "./static/media/production.076e7b4e.mp4"
+  },
+  {
+    "revision": "5fcbd827960d0a77cc16ddeabb8dbad5",
+    "url": "./static/media/realhousey.5fcbd827.jpg"
+  },
+  {
+    "revision": "0c4781dd4826171afabb142eff95e408",
+    "url": "./static/media/recreate-mode.0c4781dd.svg"
+  },
+  {
+    "revision": "a1a4dceccae3fb931de3bca6553be34c",
+    "url": "./static/media/search.a1a4dcec.jpg"
+  },
+  {
+    "revision": "b306c8f066df633165d0c89fc0eabdcd",
+    "url": "./static/media/spapshoot.b306c8f0.jpg"
+  },
+  {
+    "revision": "6f4bd69f704583540ed4a220787b6d37",
+    "url": "./static/media/tap2-hover-back.6f4bd69f.svg"
+  },
+  {
+    "revision": "f6846980e265098bba9c948a9bec3d65",
+    "url": "./static/media/tasnuva-logo.f6846980.svg"
+  },
+  {
+    "revision": "faf6795297dd2b31ebdd831b59320bfc",
+    "url": "./static/media/tasnuvaResume.faf67952.pdf"
+  },
+  {
+    "revision": "f1a9c6d4b5b01899185542ddb0a09a06",
+    "url": "./static/media/veplop.f1a9c6d4.jpg"
+  },
+  {
+    "revision": "02812878982256ae067df64db1138c79",
+    "url": "./static/media/web-code-link.02812878.svg"
+  },
+  {
+    "revision": "2f4e2ad27372b43b675b0a594b9c3242",
+    "url": "./static/media/web1.2f4e2ad2.svg"
+  }
+]);
